51faaf92ecd3ac51eb82497ccd8a363daab9586b
Silthreent <s.il3ntscript3r@gmail.com>
2020-09-05 19:44:36 -0400

    Slightly prettier main menu.

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,10 +1,13 @@
 using Godot;
-using System;
 
 public class MainMenu : MarginContainer
 {
 	Button PlayButton;
 	CheckButton StressFree;
+	LeafManager Leaves;
+
+	float Timer;
+	bool TimerRunning = false;
 
 	public override void _Ready()
 	{
@@ -12,6 +15,24 @@ public class MainMenu : MarginContainer
 		PlayButton.Connect("pressed", this, "OnPlayPressed");
 
 		StressFree = FindNode("StressFreeToggle") as CheckButton;
+
+		Leaves = FindNode("LeafManager") as LeafManager;
+		Leaves.GenerateLeaves(new LeafColor[] { LeafColor.Green, LeafColor.Blue, LeafColor.Red });
+		Leaves.Connect("LeavesDropped", this, "OnLeavesDropped");
+		Leaves.Connect("SkyLeavesDropped", this, "OnSkyLeavesDropped");
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		if (!TimerRunning)
+			return;
+
+		Timer -= delta;
+		if(Timer <= 0)
+		{
+			TimerRunning = false;
+			Leaves.DropLeaves();
+		}
 	}
 
 	void OnPlayPressed()
@@ -21,5 +42,16 @@ public class MainMenu : MarginContainer
 		GetTree().Root.AddChild(game);
 		game.ToggleStressFreeMode(StressFree.Pressed);
 		QueueFree();
+	}
+
+	void OnLeavesDropped()
+	{
+		Leaves.GenerateLeaves(new LeafColor[] { LeafColor.Green, LeafColor.Blue });
+	}
+
+	void OnSkyLeavesDropped()
+	{
+		Timer = 2.5f;
+		TimerRunning = true;
 	}
 }
